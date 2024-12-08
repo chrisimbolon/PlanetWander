68239ee7c88b0ad5bdad7c0dc9e062e5f6b36686
Christyan Simbolon <sambungdata@gmail.com>
2024-12-08 21:43:48 +0700

    First attempt on styling responsivness for Product and Pricing

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,10 +1,9 @@
-// Uses the same styles as Product
-import styles from "./Product.module.css";
+import styles from "./Pricing.module.css";
 import PageNav from "../components/PageNav";
 
-export default function Product() {
+export default function Pricing() {
   return (
-    <main className={styles.product}>
+    <main className={styles.pricing}>
       <PageNav />
       <section>
         <div>
@@ -15,16 +14,19 @@ export default function Product() {
           </h2>
           <p>
             Enjoy unlimited access to all features, free updates, and dedicated
-            support to help you wander the planet with ease.At its heart, Planet
-            Wander is more than just a travel platform — it's a community of
-            like-minded adventurers. Share your stories, exchange tips, and
+            support to help you wander the planet with ease. At its heart,
+            Planet Wander is more than just a travel platform — it's a community
+            of like-minded adventurers. Share your stories, exchange tips, and
             celebrate the joy of exploring new horizons. Whether you are a solo
             traveler or someone seeking a family-friendly getaway, Planet Wander
             connects you with a network of travelers who share your curiosity
             and love for the world. Let’s wander, discover, and grow together!
           </p>
         </div>
-        <img src="City2.png" alt="illustration of a large on top of cloud" />
+        <img
+          src="City2.png"
+          alt="illustration of a large city on top of clouds"
+        />
       </section>
     </main>
   );
