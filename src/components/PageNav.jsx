908e2670a9dd2d59fb82b48f616e18496497340e
Christyan Simbolon <sambungdata@gmail.com>
2024-12-08 14:20:44 +0700

    adding responsive navigation

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,12 +1,32 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
+import { useState } from "react";
 
 function PageNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    // setIsMenuOpen((prev) => !prev);
+    setIsMenuOpen((prev) => {
+      console.log("Menu state toggled:", !prev);
+      return !prev;
+    });
+  }
   return (
     <nav className={styles.nav}>
-      <Logo />
-      <ul>
+      <div className={styles.navContent}>
+        <Logo />
+        {/* Hamburger / Close Icon */}
+        <button
+          className={`${styles.hamburger}`}
+          onClick={toggleMenu}
+          aria-label="Toggle Menu"
+        >
+          {isMenuOpen ? "✖" : "☰"}
+        </button>
+      </div>
+      <ul className={`${styles.menu} ${isMenuOpen ? styles.show : ""}`}>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
