8130dbb7ecbee928d964c07b6b3cf8eb8a25a027
Christyan Simbolon <sambungdata@gmail.com>
2025-02-20 21:10:23 +0700

    test for SideBar.jsx by SideBar.test.jsx passed for now

---
+++ src/components/__tests__/SideBar.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import SideBar from "../SideBar";
+import { describe,  expect, } from "vitest";
+import "@testing-library/jest-dom";
+
+describe("SideBar component", () => {
+  test("renders SideBar correctly", () => {
+    render(
+      <MemoryRouter>
+        <SideBar />
+      </MemoryRouter>
+    );
+
+    // Check if Logo and AppNav are rendered
+    expect(screen.getByRole("img")).toBeInTheDocument(); // Assuming Logo contains an image
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+    
+    // Check for footer text
+    const year = new Date().getFullYear();
+    expect(screen.getByText(`© Copyright ${year} by Planet Wander`)).toBeInTheDocument();
+  });
+});
